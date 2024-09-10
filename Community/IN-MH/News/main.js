c3ff42fdecf1871a9f76b45b01a57071812a47bd
ArtInstitute <81817886+ArtInstitute@users.noreply.github.com>
2024-09-10 23:35:31 +0530

    Add files via upload

---
+++ Community/IN-MH/News/main.js
@@ -0,0 +1,24 @@
+import data from "./data-20240910.js";
+
+window.onload=(event)=>{
+    
+    document.title=data.header;
+    var date=document.getElementById('date');
+    date.innerHTML= data.date;
+    
+    
+    var h1=document.getElementById('h1');
+    h1.innerHTML= data.header;
+    
+    
+    var img=document.getElementById('img');
+    img.src= data.img;
+    
+    var cap=document.getElementById('caption');
+    cap.innerHTML= data.caption;
+    
+    
+       var article=document.getElementById('article');
+    article.innerHTML= data.article;
+    
+}
